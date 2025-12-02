e946e5c98e6566b4c9e697421fc1e092469d999e
Bjark9 <niels.martin03@gmail.com>
2025-12-02 19:11:07 +0100

    added .gitignore in root repo

--- server.py
+++ server.py
@@ -5,7 +5,7 @@ server.bind(('localhost',12345)) # navigate to server using http://localhost:123
 server.listen(5) # Listen to n numbre of connexions at once
 
 print("Server is listening on port 12345...")
-conn, addr = server.accept()
+conn, addr = server.accept() # Connection, address
 print(f"Connection established with {addr}")
 
 while True:
@@ -13,3 +13,11 @@ while True:
     if msg.lower() == 'exit':
         print("Client has exited the chat :P")
         break
+    print(f"Client: {msg}")
+    response = input("You: ")
+    conn.send(response.encode())
+    conn.close()
+    if response.lower() == "exit":
+        print("You have exited the chat.")
+        break
+conn.close() 
