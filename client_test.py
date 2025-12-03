a476a20a4c7366c56111dbf383f08ef845daf20c
Bjark9 <niels.martin03@gmail.com>
2025-12-03 11:52:04 +0100

    broadcast of clients message

---
+++ client_test.py
@@ -0,0 +1,26 @@
+import asyncio
+import websockets
+import datetime
+
+async def chat():
+    uri = "ws://127.0.0.1:6789"
+
+    id = datetime.datetime.now()
+
+    async with websockets.connect(uri) as websocket:
+        print(f"{id} Connected to server!")
+
+        async def listen():
+            while True:
+                msg = await websocket.recv()
+                print(f"[Server] {msg}")
+
+        async def send():
+            while True:
+                # run input() in a thread so it doesn't block asyncio
+                msg = await asyncio.to_thread(input)
+                await websocket.send(msg)
+
+        await asyncio.gather(listen(), send())
+
+asyncio.run(chat())
--- server.py
+++ server.py
@@ -1,25 +1,26 @@
 import asyncio 
 import websockets as ws
 
-#  
-connected_clients = set()
-async def handle_client(websocket, path):
+connected_clients = set() # Build unordered object of unique elements
+# This function is called each time a new client connects
+async def handle_client(websocket):
     # Register the new client
     connected_clients.add(websocket)
     try:
+        # Message loop, recieve messages from this client. This loop runs as long as client stays connected
         async for message in websocket:
-            # Broadcast the message to all connected clients
-            await asyncio.wait([client.send(message)] for client in connected_clients)
+            # Broadcast the message to all connected clients, build a list of stacks and each stack sends the message to a different connected client (asyncio.wait to run them all at the same time)
+            await asyncio.gather(*(client.send(message) for client in connected_clients), return_exceptions=True)
+    # If client disconnect -> async loop ends and finally block gets executed
     finally:
         # Unregister the client
         connected_clients.remove(websocket)
 
 # Function to start the WebSocket server
-async def main():
-    server = await ws.serve(handle_client,"localhost",6789) # Set up WebSocket server that listens on port 6789
-                                                            # When a client connects, server will handle the connection using handle_client function
-                                                            # TODO: create static IP ?
-    await server.wait_closed()
+async def main():                                      
+    async with ws.serve(handle_client,"0.0.0.0",6789):      # Set up WebSocket server that listens on port 6789
+        await asyncio.Future() # Run forever                # Every time a client connects, server will handle the connection 
+                                                            # using the handle_client function              
 
 if __name__ == "__main__":
     asyncio.run(main())
