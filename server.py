1b4249832e68d69d0f21aa7fb06ae58bf8273f7c
Bjark9 <niels.martin03@gmail.com>
2025-12-09 22:01:43 +0100

    updated server.py

--- server.py
+++ server.py
@@ -4,6 +4,8 @@ import json
 import datetime
 import logging
 
+# TODO: change logging system, maybe override old logging file so we don't get an infinite long .log file
+
 # Notes:
 # ° variable "websocket" is a websocket connection object that represents one connected client (like an ID)
 # ° List of actions: "createRoom, joinRoom, leaveRoom, sendMessage, receiveMessage, identify, rename"
@@ -52,7 +54,7 @@ async def handle_client(websocket):
     logging.info(f"Connected clients: {len(connected_clients)}")
     
     # Give random username to client
-    users[websocket] = datetime.datetime.now() # TODO: change this to proper username (maybe strip or something like that)
+    users[websocket] = "User_" + str(datetime.datetime.now().timestamp())
     logging.info(f"User: {users[websocket]}")
 
     # Add client to "default" room.
@@ -72,21 +74,31 @@ async def handle_client(websocket):
             # Listen to the action
             match action.get("action"):
                 case "createRoom":
-                    if action["room"] and action["room"] not in rooms:
-                        rooms[action["room"]] = set()
+                    room = action.get("room")
+                    if room and room not in rooms:
+                        rooms[room] = set()
                         logging.info(f"Created room: {action['room']}")
-                
+                        # Broadcast 
+                        for client in connected_clients:
+                            await sendjson(client, {"action": "roomsList", "rooms": list(rooms.keys())})
+
                 case "joinRoom":
                     # Join room
-                    if action["room"] not in rooms:
-                        await sendjson(websocket, {"action": "error", "message": f"Room '{action["room"]}' does not exist."}) 
+                    room = action.get("room")
+                    if room not in rooms:
+                        await sendjson(websocket, {"action": "error", "message": f"Room '{room}' does not exist."}) 
                         continue
 
                     rooms[current_room].remove(websocket)
-                    rooms[action["room"]].add(websocket)
+                    rooms[room].add(websocket)
 
-                    client_rooms[websocket] = action["room"]
-                    current_room = client_rooms[websocket]
+                    client_rooms[websocket] = room
+                    current_room = room
+
+                    await sendjson(websocket, {
+                        "action": "joined",
+                        "payload": {"room": room}
+                    })
 
                 case "leaveRoom":
                     # Leave current room and join default room
@@ -97,35 +109,51 @@ async def handle_client(websocket):
                         current_room = "default"
                         logging.info("Client left room and joined default.")
 
+                    await sendjson(websocket, {
+                        "action": "left",
+                        "payload": {"room": current_room} # TODO: change current_room to previous room
+                    })
+
                 case "sendMessage":
                     msg = action.get("message")
                     if not msg:
                         continue # Ignore empty messages
 
                     # Broadcast message to everyone in the client's current_room
-                    message_obj = {"action": "message", "sender": str(websocket.remote_address), "message": msg, "room": current_room}
-                    
-                    # Create a list of send tasks, excluding the sender
-                    send_tasks = [client.send(json.dumps(message_obj)) 
-                                  for client in rooms[current_room] if client != websocket]
-                    
-                    await asyncio.gather(*send_tasks, return_exceptions=True)
+                    message_obj = {
+                        "action": "message",
+                        "payload": {
+                            "from": users.get(websocket, "Unknown"),
+                            "room": current_room,
+                            "message": msg
+                        }
+                    }
+                                        
+                    for client in rooms[current_room]:
+                        if client != websocket:
+                            await client.send(json.dumps(message_obj))
 
                 case "receiveMessage":
                     msg = action["message"] 
 
                 case "identify":
-                    ...
+                    # Identify user
+                    username = action.get("payload", {}).get("username", "")
+                    if username:
+                        users[websocket] = username
                 
                 case "rename":
                     ...
+
                 case "roomsList":
                     await sendjson(websocket, {"action": "roomsList", "rooms": list(rooms.keys())})
+
                 case _:
                     print("Not an action...")
                     logging.error("Not an action...")
     except Exception as e:
         logging.exception(f"Error handling client: {e}")
+
     # If client disconnect -> async loop ends and finally block gets executed
     finally:
         # Remove client from rooms
